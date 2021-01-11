8c9474af53736c1abc4ba4cbeb3e01e4c5f9f40a
JavMig9903 <javiermiguel9903@gmail.com>
2021-01-11 19:42:26 +0100

    final

--- app/src/main/java/com/example/ejemploadapterconlista/MainActivity.kt
+++ app/src/main/java/com/example/ejemploadapterconlista/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ejemploadapterconlista.databinding.ActivityMainBinding
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,13 +15,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         createRecyclerView()
     }
 
     private fun createRecyclerView() {
-        val listaString = listOf("Elemento1","Elemento2","Elemento3","Elemento4")
-        adapter = StringAdapter(listaString.toMutableList())
+        val laListadelLoro = mutableListOf<String>()
+        var random = Random
+        var inicio= random.nextInt(5..10)
+        for (i in 0..inicio){
+            laListadelLoro.add("PC-${i}")
+        }
+
+        adapter = StringAdapter(laListadelLoro.toMutableList())
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = adapter
     }
--- app/src/main/java/com/example/ejemploadapterconlista/StringAdapter.kt
+++ app/src/main/java/com/example/ejemploadapterconlista/StringAdapter.kt
@@ -4,44 +4,93 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 class StringAdapter(var listaString : MutableList<String>) : RecyclerView.Adapter<StringAdapter.StringViewHolder>()  {
-
-    class StringViewHolder(var root: View, var textView: TextView) : RecyclerView.ViewHolder(root)
-
+    class StringViewHolder(var root: View, var textView: TextView, var check: CheckBox) : RecyclerView.ViewHolder(root)
+    var listaBool = mutableListOf<Boolean>()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StringViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
         val twTextView = view.findViewById<TextView>(R.id.textView)
-        return StringViewHolder(view, twTextView)
+        val check1 = view.findViewById<CheckBox>(R.id.checkBox1)
+        return StringViewHolder(view, twTextView, check1)
     }
 
     override fun getItemCount(): Int {
-        return listaString.size + 1
+        return listaString.size+1
     }
 
     override fun onBindViewHolder(holder: StringViewHolder, position: Int) {
-        if (position == itemCount -1) {
-            holder.textView.text = "Añadir"
+        holder.textView.setBackgroundColor(holder.textView.context.getColor(R.color.teal_700))
+        when (position){
+        0 ->{
+            holder.textView.text = "Borrar"
             holder.root.setOnClickListener {
-                val toast = Toast.makeText(it.context, "Añadiendo...", Toast.LENGTH_LONG)
+                val toast = Toast.makeText(it.context, "Borrando...", Toast.LENGTH_LONG)
                 toast.setGravity(Gravity.CENTER,0,0)
                 toast.show()
-                listaString.add("Elemento$position")
+                var random = Random
+                var dropitem = random.nextInt(1..listaString.size-2)
+                listaString.removeAt(dropitem)
+                listaBool.removeAt(dropitem)
+            }
+            return
+        }
+        itemCount -1 ->{
+            holder.textView.text = "Insertar"
+            holder.root.setOnClickListener {
+                val toast = Toast.makeText(it.context, "Añadiendo PC-${position}", Toast.LENGTH_LONG)
+                toast.setGravity(Gravity.CENTER,0,0)
+                toast.show()
+                listaString.add("PC-${position+1}")
                 notifyDataSetChanged()
             }
             return
         }
-        if (position%2 == 0) {
-            holder.textView.text = listaString[position]
-            holder.textView.setBackgroundColor(holder.textView.context.getColor(R.color.purple_200))
-        } else {
-            holder.textView.text = "${listaString[position]} y Soy Impar"
-            holder.textView.setBackgroundColor(holder.textView.context.getColor(R.color.teal_700))
+        itemCount -2 ->{
+            holder.textView.text = "Contar Encendidos"
+            holder.root.setOnClickListener {
+                var count=0
+                listaBool.forEach {
+                    if(it==true){
+                        count++
+                    }
+                }
+                val toast = Toast.makeText(it.context, "Hay $count seleccionados", Toast.LENGTH_LONG)
+                toast.setGravity(Gravity.CENTER,0,0)
+                toast.show()
+            }
+            return
         }
-
+        else -> {
+        }
+    }
+    var random = Random
+    var checked = random.nextInt(1..2)
+    if (checked==1) {
+        holder.check.isChecked = true
+        listaBool.add(true)
+        holder.textView.setBackgroundColor(holder.textView.context.getColor(R.color.verde))
+    }else {
+        holder.check.isChecked = false
+        listaBool.add(false)
+        holder.textView.setBackgroundColor(holder.textView.context.getColor(R.color.rojo))
+    }
+    holder.check.setOnCheckedChangeListener { buttonView, isChecked ->
+        if (isChecked){
+            holder.textView.setBackgroundColor(holder.textView.context.getColor(R.color.verde))
+            listaBool.set(position,true)
+        }else{
+            holder.textView.setBackgroundColor(holder.textView.context.getColor(R.color.rojo))
+            listaBool.set(position,false)
+        }
+    }
+    holder.textView.setText(listaString[position])
     }
 
 }
